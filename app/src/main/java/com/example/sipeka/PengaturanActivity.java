8943175dbfbeef490fc8d4d50ae9e62a41bbffd9
mrrest017 <55204773+mrrest017@users.noreply.github.com>
2020-04-26 13:38:36 +0700

    pilih galeri beres direwangi pak wir

--- app/src/main/java/com/example/sipeka/PengaturanActivity.java
+++ app/src/main/java/com/example/sipeka/PengaturanActivity.java
@@ -6,49 +6,64 @@ import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.SwitchCompat;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class PengaturanActivity extends AppCompatActivity {
 
-    SwitchCompat switchCompat;
+    private Switch darkModeSwitch;
+    private TextView logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES) {
-            //setTheme(R.style.DarkTheme);
-        //}
-        //else
-            //setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
+        if(new DarkModePrefManager(this).isNightMode()){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
         setContentView(R.layout.activity_pengaturan);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        switchCompat = findViewById(R.id.switch_dark_mode);
+        logout = findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPreferences=getSharedPreferences("remember",MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.commit();
 
-        //if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES) {
-            //switchCompat.setChecked(true);
-        //}
-        //switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            //@Override
-            //public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                //if (isChecked) {
-                    //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                    //restartApp();
-                //}
-                //else {
-                    //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                    //restartApp();
-                //}
-            //}
-        //});
+                finish();
+            }
+        });
 
     }
 
-    //public void restartApp () {
-        //Intent intent = new Intent(getApplicationContext(),PengaturanActivity.class);
-        //startActivity(intent);
-        //finish();
-    //}
+    public void setDarkMode(Window window){
+        if(new DarkModePrefManager(this).isNightMode()){
+//        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            changeStatusBar(0,window);
+        }else{
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            changeStatusBar(1,window);
+        }
+    }
+    public void changeStatusBar(int mode, Window window){
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.contentBodyColor));
+//white mode
+            if(mode==1){
+                window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            }
+        }
+    }
 }
