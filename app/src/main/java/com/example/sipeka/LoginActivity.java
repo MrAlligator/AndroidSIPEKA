666f890b8b95f9b3714a496b0ae1911f412348c5
mrrest017 <55204773+mrrest017@users.noreply.github.com>
2020-04-27 15:54:47 +0700

    preferences

--- app/src/main/java/com/example/sipeka/LoginActivity.java
+++ app/src/main/java/com/example/sipeka/LoginActivity.java
@@ -45,7 +45,6 @@ public class LoginActivity extends AppCompatActivity {
         mViewPassword = findViewById(R.id.password);
         lupa = findViewById(R.id.lupa);
 
-        Toast.makeText(getApplicationContext(), "User Login Status : " + session.isLoggedIn(), Toast.LENGTH_LONG).show();
 
         showpass.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,30 +63,24 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        mViewPassword.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE || actionId == EditorInfo.IME_NULL) {
+                    razia();
+                    return true;
+                }
+                return false;
+            }
+        });
+
         masuk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = mViewUser.getText().toString();
-                String password = mViewPassword.getText().toString();
-
-                if(username.trim().length() > 0 && password.trim().length() > 0){
-
-                    if(username.equals("admin") && password.equals("admin")){
-
-                        session.createLoginSession("Android Hive", "anroidhive@gmail.com");
-
-                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
-                        startActivity(i);
-                        finish();
-
-                    }else{
-                        showDialog2();
-                    }
-                }else{
-                    showDialog3();
-                }
+                razia();
             }
         });
+
     }
 
     private void showDialog3(){
@@ -149,6 +142,72 @@ public class LoginActivity extends AppCompatActivity {
 
         AlertDialog alertDialog = alertDialogBuilder.create();
         alertDialog.show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (Preferences.getLoggedInStatus(getBaseContext())){
+            startActivity(new Intent(getBaseContext(),MainActivity.class));
+            finish();
+        }
+    }
+
+    /** Men-check inputan User dan Password dan Memberikan akses ke MainActivity */
+    private void razia(){
+        /* Mereset semua Error dan fokus menjadi default */
+        mViewUser.setError(null);
+        mViewPassword.setError(null);
+        View fokus = null;
+        boolean cancel = false;
+
+        /* Mengambil text dari form User dan form Password dengan variable baru bertipe String*/
+        String user = mViewUser.getText().toString();
+        String password = mViewPassword.getText().toString();
+
+        /* Jika form user kosong atau TIDAK memenuhi kriteria di Method cekUser() maka, Set error
+         *  di Form User dengan menset variable fokus dan error di Viewnya juga cancel menjadi true*/
+        if (TextUtils.isEmpty(user)){
+            mViewUser.setError("This field is required");
+            fokus = mViewUser;
+            cancel = true;
+        }else if(!cekUser(user)){
+            mViewUser.setError("This Username is not found");
+            fokus = mViewUser;
+            cancel = true;
+        }
+
+        /* Sama syarat percabangannya dengan User seperti di atas. Bedanya ini untuk Form Password*/
+        if (TextUtils.isEmpty(password)){
+            mViewPassword.setError("This field is required");
+            fokus = mViewPassword;
+            cancel = true;
+        }else if (!cekPassword(password)){
+            mViewPassword.setError("This password is incorrect");
+            fokus = mViewPassword;
+            cancel = true;
+        }
+
+        /* Jika cancel true, variable fokus mendapatkan fokus */
+        if (cancel) fokus.requestFocus();
+        else masuk();
+    }
+
+    /** Menuju ke MainActivity dan Set User dan Status sedang login, di Preferences */
+    private void masuk(){
+        Preferences.setLoggedInUser(getBaseContext(),Preferences.getRegisteredUser(getBaseContext()));
+        Preferences.setLoggedInStatus(getBaseContext(),true);
+        startActivity(new Intent(getBaseContext(),MainActivity.class));finish();
+    }
+
+    /** True jika parameter password sama dengan data password yang terdaftar dari Preferences */
+    private boolean cekPassword(String password){
+        return password.equals(Preferences.getRegisteredPass(getBaseContext()));
+    }
+
+    /** True jika parameter user sama dengan data user yang terdaftar dari Preferences */
+    private boolean cekUser(String user){
+        return user.equals(Preferences.getRegisteredUser(getBaseContext()));
     }
 
 }
