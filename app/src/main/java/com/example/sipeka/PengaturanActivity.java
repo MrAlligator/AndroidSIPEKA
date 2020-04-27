b59945aea26c96799a8818e2462cfcf816858f1b
mrrest017 <55204773+mrrest017@users.noreply.github.com>
2020-04-27 11:51:10 +0700

    gak ruh :v

--- app/src/main/java/com/example/sipeka/PengaturanActivity.java
+++ app/src/main/java/com/example/sipeka/PengaturanActivity.java
@@ -4,11 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatCallback;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.SwitchCompat;
-
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -21,46 +16,20 @@ import android.widget.TextView;
 public class PengaturanActivity extends AppCompatActivity {
 
     private Switch darkModeSwitch;
-    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(new DarkModePrefManager(this).isNightMode()){
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-        }
         setContentView(R.layout.activity_pengaturan);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        setDarkMode(getWindow());
         darkModeSwitch = findViewById(R.id.darkModeSwitch);
         darkModeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                DarkModePrefManager darkModePrefManager = new DarkModePrefManager(mContext);
-                darkModePrefManager.setDarkMode(!darkModePrefManager.isNightMode());
-                recreate();
+
             }
         });
 
-    }
-
-    public void setDarkMode(Window window){
-        if(new DarkModePrefManager(this).isNightMode()){
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-            changeStatusBar(0,window);
-        }else{
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-            changeStatusBar(1,window);
-        }
-    }
-    public void changeStatusBar(int mode, Window window){
-        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(this.getResources().getColor(R.color.contentBodyColor));
-            if(mode==1){
-                window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-            }
-        }
     }
 }
