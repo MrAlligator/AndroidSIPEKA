f28536799b3569783efc175c1d83d54ecda37f7c
MrAlligator <rizkiw8778@gmail.com>
2020-06-28 21:12:35 +0700

    Aku Nggarap Penduduk minus Tambah karo Edit Uangel Njr

--- app/src/main/java/com/example/sipeka/LihatPenduduk.java
+++ app/src/main/java/com/example/sipeka/LihatPenduduk.java
@@ -2,20 +2,27 @@ package com.example.sipeka;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.sipeka.Model.Ktp.PostPutDelKtp;
 import com.example.sipeka.Rest.ApiClient;
 import com.example.sipeka.Rest.ApiInterface;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class LihatPenduduk extends AppCompatActivity {
-    EditText edtNik, edtNama, edtNoKK, edtTanggal, edtAlamat, edtPekerjaan, edtBerlaku;
+    EditText edtNik, edtNama, edtNoKK, edtTanggal, edtAlamat, edtPekerjaan, edtBerlaku, edtJenkel, edtGoldar, edtKode, edtRw, edtRt, edtAgama, edtStatus, edtKewarganegaraan;
     ApiInterface mApiInterface;
     AutoCompleteTextView edttmptLahir, edtKelurahan, edtKecamatan;
-    Spinner edtJenkel;
+    Button btDelete, btUpdate;
 
 
     @Override
@@ -35,6 +42,13 @@ public class LihatPenduduk extends AppCompatActivity {
         edtKelurahan = findViewById(R.id.txtKelurahan);
         edtKecamatan = findViewById(R.id.txtKecamatan);
         edtJenkel = findViewById(R.id.txtJenkel);
+        edtAgama = findViewById(R.id.txtAgama);
+        edtGoldar = findViewById(R.id.txtGoldar);
+        edtKode = findViewById(R.id.txtRT);
+        edtRt = findViewById(R.id.txtRT1);
+        edtRw = findViewById(R.id.txtRT2);
+        edtStatus = findViewById(R.id.txtStatus);
+        edtKewarganegaraan = findViewById(R.id.txtKewarganegaraan);
         edttmptLahir = (AutoCompleteTextView) findViewById(R.id.txtTmptLahir);
         Intent mIntent = getIntent();
         edtNik.setText(mIntent.getStringExtra("NIK"));
@@ -49,60 +63,41 @@ public class LihatPenduduk extends AppCompatActivity {
         edtKecamatan.setText(mIntent.getStringExtra("Kecamatan"));
         edtPekerjaan.setText(mIntent.getStringExtra("Pekerjaan"));
         edtBerlaku.setText(mIntent.getStringExtra("Berlaku Hingga"));
+        edtJenkel.setText(mIntent.getStringExtra("Jenis Kelamin"));
+        edtAgama.setText(mIntent.getStringExtra("Agama"));
+        edtGoldar.setText(mIntent.getStringExtra("Gol Darah"));
+        edtKode.setText(mIntent.getStringExtra("KodeRT"));
+        edtStatus.setText(mIntent.getStringExtra("Status Perkawinan"));
         mApiInterface = ApiClient.getClient().create(ApiInterface.class);
-//        btUpdate = (Button) findViewById(R.id.btUpdate2);
-//        btUpdate.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Call<PostPutDelKtp> updateKtpCall = mApiInterface.putKtp(
-//                        edtNik.getText().toString(),
-//                        edtNama.getText().toString(),
-//                        edtrw.getText().toString());
-//                ((Call) updateKtpCall).enqueue(new Callback<PostPutDelKtp>() {
-//                    @Override
-//                    public void onResponse(Call<PostPutDelKtp> call, Response<PostPutDelKtp> response) {
-//                        MainActivity.ma.refresh();
-//                        finish();
-//                    }
-//
-//                    @Override
-//                    public void onFailure(Call<PostPutDelKtp> call, Throwable t) {
-//                        Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
-//                    }
-//                });
-//            }
-//        });
-//        btDelete = (Button) findViewById(R.id.btDelete2);
-//        btDelete.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (edtNik.getText().toString().trim().isEmpty()==false){
-//                    Call<PostPutDelKtp> deleteKtp = mApiInterface.deleteKtp(edtNik.getText().toString());
-//                    deleteKtp.enqueue(new Callback<PostPutDelKtp>() {
-//                        @Override
-//                        public void onResponse(Call<PostPutDelKtp> call, Response<PostPutDelKtp> response) {
-//                            MainActivity.ma.refresh();
-//                            finish();
-//                        }
-//
-//                        @Override
-//                        public void onFailure(Call<PostPutDelKtp> call, Throwable t) {
-//                            Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
-//                        }
-//                    });
-//                }else{
-//                    Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
-//                }
-//            }
-//        });
-//        btBack = (Button) findViewById(R.id.btBackGo);
-//        btBack.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                MainActivity.ma.refresh();
-//                finish();
-//            }
-//        });
-//    }
+        btUpdate = (Button) findViewById(R.id.Edit);
+        btUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LihatPenduduk.this, EditPendActivity.class));
+            }
+        });
+        btDelete = (Button) findViewById(R.id.Hapus);
+        btDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtNik.getText().toString().trim().isEmpty()==false){
+                    Call<PostPutDelKtp> deleteKtp = mApiInterface.deleteKtp(edtNik.getText().toString());
+                    deleteKtp.enqueue(new Callback<PostPutDelKtp>() {
+                        @Override
+                        public void onResponse(Call<PostPutDelKtp> call, Response<PostPutDelKtp> response) {
+                            PendudukActivity.ma.refresh();
+                            finish();
+                        }
+
+                        @Override
+                        public void onFailure(Call<PostPutDelKtp> call, Throwable t) {
+                            Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }else{
+                    Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
